e7d934b9514fe275909b7d3f200f780ca1e75060
Paz Valenzuela <pazvalenzu@gmail.com>
2023-07-04 12:16:37 -0700

    users

--- src/routes/users.py
+++ src/routes/users.py
@@ -10,3 +10,18 @@ def list_users():
     users = list(map(lambda user: user.serialize(), users))
 
     return jsonify(users), 200
+
+@api.route('/formulario', methods=['POST'])
+def formulario_user():
+    data=request.get_json()
+    new_user = Usuario() 
+    new_user.username = data['nombre']
+    new_user.email=data['correo']
+    new_user.password=data['contraseña']
+    new_user.id_habilidades =data ["habilidades.id"]
+    new_user.id_interes = data["habilidades.id"]
+    new_user.id_roles =data ["roles.id"]
+    new_user.save()
+
+
+    return jsonify(message='Ya puedes disfrutar de FiDi'), 201
