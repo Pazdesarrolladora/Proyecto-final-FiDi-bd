055cb018d7894f5606e4823ac6ec705d1a0763db
Mirko Pasten <mikish_ash@hotmail.com>
2023-07-07 06:00:37 -0400

    Backend actualizado para renderizar perfil propio

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,18 +1,19 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from models.usuario import Usuario
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 
 api = Blueprint('api_auth', __name__)
 
-@api.route('/login', methods=['POST'])
+@api.route('/loginform', methods=['POST'])
 def login():
     
     correo = request.json.get('correo')
     password = request.json.get('password')
     
     if not correo: return jsonify({ "correo": "El correo es requerido!"}), 400
-    if not password: return jsonify({ "password": "Password is required!"}), 400
+    if not password: return jsonify({ "password": "La contraseña es requerido!"}), 400
     
     userFound = Usuario.query.filter_by(correo=correo).first()
     
@@ -21,17 +22,19 @@ def login():
     if not check_password_hash(userFound.password, password):
         return jsonify({ "message": "El correo o password es incorrecto"}), 401
     
-    access_token = create_access_token(identity=userFound["id"])
+    access_token = create_access_token(identity=userFound.id)
     
     data = {
         "access_token": access_token,
         "user": userFound.serialize()
     }
     
-    return jsonify(data), 200
+    return jsonify({ "success": "Login successfully", "status": 200, "data": data}), 200
     
     
-@api.route('/register', methods=['POST'])
-def register():
-    # code to register users
-    pass
+@api.route('/profile', methods=['GET'])
+@jwt_required() # Definiendo una ruta privada
+def profiles():
+    id = get_jwt_identity()
+    userFound = Usuario.query.get(id)
+    return jsonify({ "message": "Private Route", "usuario": userFound.serialize() }), 200
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,8 +1,11 @@
 import cloudinary
 import cloudinary.uploader
 from flask import Blueprint, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from models.usuario import Usuario
 from models.imagen import Imagen
+from models.habilidad import Habilidad
+from models.usuario import habilidad_usuario
 
 api = Blueprint('api_users', __name__)
 
@@ -20,17 +23,22 @@ def registrar_usuario():
     correo = request.form['correo']
     nombre = request.form['nombre']
     password = request.form['password']
-    habilidades = request.form['habilidades']
     imagen = None
 
+    #Valido si los campos vienen con informacion
     if not correo: return jsonify({"message": "El correo es requerido!"}), 400
     if not nombre: return jsonify({"message": "Nombre requerido!"}), 400
     if not password: return jsonify({"message": "Password requerido!"}), 400
-    if not habilidades: return jsonify({"message": "Habilidades requeridas!"}), 400
     if not 'imagen' in request.files: 
         return jsonify({"message": "Image is required!"}), 400
     else: 
         imagen = request.files['imagen']
+
+    #Creo una variable userFound, a la cual le asigno el primer usuario que encuentre con la funcion filter_by
+    #buscando en la db, el username provisto por el POST dentro de todos los User existentes
+    userFound = Usuario.query.filter_by(correo=correo).first()
+    #Si el usuario encontrado efectivamente existe, se devuelve un mensaje
+    if userFound: return jsonify({"message": "El correo ya esta registrado!"}), 400
 
     response = cloudinary.uploader.upload(imagen, folder="imagenesFidi")
     
@@ -44,9 +52,17 @@ def registrar_usuario():
     new_user = Usuario()
     new_user.correo = correo
     new_user.nombre = nombre
-    new_user.password = password
+    new_user.password = generate_password_hash(password)
     new_user.src_imagen = response['secure_url']
+
+    # for id in habilidades:
+    #     habilidad = Habilidad.query.get(id)
+    #     if habilidad: new_user.habilidades.append(habilidad)
+
+    # for id in intereses:
+    #     interes = Habilidad.query.get(id)
+    #     if interes: new_user.habilidades.append(interes)
+
     new_user.save()
 
-
-    return jsonify(message='Ya puedes disfrutar de FiDi'), 201
+    return jsonify({"success": "User created successfully", "status": 201}), 201
