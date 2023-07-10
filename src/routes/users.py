fc35aa185dd6c7f2d287ff1ab53ba60edb436ca6
Mirko Pasten <mikish_ash@hotmail.com>
2023-07-09 22:00:42 -0400

    Sin cambios y tabla habilidades con categorias

--- src/routes/users.py
+++ src/routes/users.py
@@ -23,19 +23,19 @@ def registrar_usuario():
     correo = request.form['correo']
     nombre = request.form['nombre']
     password = request.form['password']
-    habilidades = request.form['habilidades'] #Recibo el array habilidades mandados por el front, los id's de las habilidades seleccionadas por el usuario
-    intereses = request.form['intereses'] #Recibo el array intereses mandados por el front, los id's de las intereses seleccionadas por el usuario
+    #habilidades = request.form['habilidades'] #Recibo el array habilidades mandados por el front, los id's de las habilidades seleccionadas por el usuario
+    #intereses = request.form['intereses'] #Recibo el array intereses mandados por el front, los id's de las intereses seleccionadas por el usuario
     imagen = None
 
-    print(habilidades) #Confirmado de que recibo id's
-    print(intereses)
+    #print(habilidades) #Confirmado de que recibo id's
+    #print(intereses)
 
     #Valido si los campos vienen con informacion
     if not correo: return jsonify({"advertencia": "El correo es requerido!"}), 400
     if not nombre: return jsonify({"advertencia": "Nombre requerido!"}), 400
     if not password: return jsonify({"advertencia": "Password requerido!"}), 400
-    if not habilidades: return jsonify({"advertencia": "Habilidades requeridas!"}), 400
-    if not intereses: return jsonify({"advertencia": "Intereses requeridos!"}), 400
+    #if not habilidades: return jsonify({"advertencia": "Habilidades requeridas!"}), 400
+    #if not intereses: return jsonify({"advertencia": "Intereses requeridos!"}), 400
     if not 'imagen' in request.files: 
         return jsonify({"advertencia": "La imagen es requerida!"}), 400
     else: 
@@ -62,17 +62,17 @@ def registrar_usuario():
     new_user.password = generate_password_hash(password)
     new_user.src_imagen = response['secure_url']
 
-    for id in habilidades:
-        habilidad = Habilidad.query.get(id)
-        if habilidad and habilidad not in new_user.habilidades:
-            new_user.habilidades.append(habilidad.id)
+    # for id in habilidades:
+    #     habilidad = Habilidad.query.get(id)
+    #     if habilidad and habilidad not in new_user.habilidades:
+    #         new_user.habilidades.append(habilidad.id)
 
-    #Recorro este array intereses, y por cada iteracion asigno a 'interes' la busqueda de la habilidad por id de cada vuelta
-    for id in intereses:
-        interes = Habilidad.query.get(id)
-        #Si NO existe el interes buscado por id, se crea un nuevo elemento en la tabla que incluye el id del usuario y el id de la habilidad (clave compuesta) por cada iteracion
-        if interes and interes not in new_user.habilidades:
-            new_user.habilidades.append(interes.id)
+    # #Recorro este array intereses, y por cada iteracion asigno a 'interes' la busqueda de la habilidad por id de cada vuelta
+    # for id in intereses:
+    #     interes = Habilidad.query.get(id)
+    #     #Si NO existe el interes buscado por id, se crea un nuevo elemento en la tabla que incluye el id del usuario y el id de la habilidad (clave compuesta) por cada iteracion
+    #     if interes and interes not in new_user.habilidades:
+    #         new_user.habilidades.append(interes.id)
     
     new_user.save()
 
