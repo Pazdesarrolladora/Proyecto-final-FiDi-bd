c3d86d39f1c4d30fe92751917a7bf42faec61ac1
Mirko Pasten <mikish_ash@hotmail.com>
2023-07-09 21:42:41 -0400

    Actualizada la tabla de habilidades junto con las clases

--- src/models/habilidad.py
+++ src/models/habilidad.py
@@ -3,12 +3,14 @@ from models import db
 class Habilidad(db.Model):
     __tablename__ = "habilidades"
     id = db.Column(db.Integer, primary_key=True)
-    descripcion = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.String(200), nullable=False)
+    categoria = db.Column(db.String(100))
 
     def serialize(self):
         return {
             "id": self.id,
-            "descripcion": self.descripcion
+            "descripcion": self.descripcion,
+            "categoria": self.categoria
         }
         
     def save(self):
