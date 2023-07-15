a012d3d8ca644e630ed07f35b0591934444647c3
Paz Valenzuela <pazvalenzu@gmail.com>
2023-07-15 15:59:03 -0700

    commit

--- src/models/match.py
+++ src/models/match.py
@@ -2,16 +2,16 @@ from models import db
 
 class Match(db.Model):
     __tablename__ = "matches" #Al hacer match debe haber una funcion que compruebe si existe el opuesto, ejemplo, (id propio = 1) (id match = 3) (estado = 1)| preguntar si existe (id propio=3) (id match = 1) (estado = 1) si es true habra match
-    id_match = db.Column(db.Integer, db.ForeignKey("usuarios.id"), primary_key=True) #Id persona match
-    id_propio = db.Column(db.Integer, db.ForeignKey("usuarios.id"), primary_key=True) #Id propio, dueño de la cuenta
+    id =  db.Column(db.Integer, primary_key=True)
+    id_propio = db.Column(db.Integer, db.ForeignKey("usuarios.id")) 
     estado = db.Column(db.Integer) # estado=1 Like, estado=2 Siguiente
 
-    def serialize(self):
-        return {
-            "id_match": self.id_match,
-            "id_propio": self.id_propio,
-            "estado": self.estado
-        }
+    # def serialize(self):
+    #     return {
+    #         "id_match": self.id_match,
+    #         "id_propio": self.id_propio,
+    #         "estado": self.estado
+    #     }
         
     def save(self):
         db.session.add(self)
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -79,7 +79,7 @@ def listarUsuarios():
     return jsonify({ "message": "Private Route", "usuario": userFound }), 200
 
 @api.route('/GuardarMatch', methods=['POST'])
-# @jwt_required() # Definiendo una ruta privada
+@jwt_required() # Definiendo una ruta privada
 def guardarMatch():
     print("Quiero entrar, eso es lo mejor")
     data = request.json
